211fb72a41adf6612842c77cb85aef3baf82bc7d
Alexis Massiaux <alexis.massiaux@worldline.com>
2025-05-01 19:48:48 +0200

    feat: add record component to app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {RecordTableComponent} from './record-table/record-table.component';
 
 @Component({
   selector: 'app-root',
+  imports:[RecordTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
