482e4006c1c6ec0f323bc2c385c3f50b05465091
Alexis Massiaux <alexis.massiaux@worldline.com>
2025-05-01 19:40:24 +0200

    feat: init record table component

---
+++ src/app/record-table/record-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-record-table',
+  imports: [],
+  templateUrl: './record-table.component.html',
+  styleUrl: './record-table.component.scss'
+})
+export class RecordTableComponent {
+
+}
