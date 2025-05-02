7da9a5f521f75bc46d213efbd358b3a6cd101690
Alexis Massiaux <alexis.massiaux@worldline.com>
2025-05-02 10:55:41 +0200

    feat: add tab filter on 2 specific columns

--- src/app/record-table/record-table.component.ts
+++ src/app/record-table/record-table.component.ts
@@ -1,6 +1,8 @@
 import {AfterViewInit, Component, ViewChild} from '@angular/core';
 import {MatTableDataSource, MatTableModule} from '@angular/material/table';
 import {MatSort, MatSortModule} from '@angular/material/sort';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 import {Record} from './record.model'
 
@@ -10,7 +12,7 @@ const RECORDS_DATA: Record[] = [
 
 @Component({
   selector: 'app-record-table',
-  imports: [MatTableModule, MatSortModule],
+  imports: [MatTableModule, MatSortModule, MatInputModule, MatFormFieldModule],
   templateUrl: './record-table.component.html',
   styleUrl: './record-table.component.scss'
 })
@@ -21,8 +23,23 @@ export class RecordTableComponent implements AfterViewInit {
 
   @ViewChild(MatSort) sort!: MatSort;
 
+  ngOnInit() {
+    this.dataSource.filterPredicate = this.customFilterPredicate.bind(this);
+  }
+
+  customFilterPredicate(data: Record, filter: string): boolean {
+    const filterValue = filter.trim().toLowerCase();
+    return data.name.toLowerCase().includes(filterValue) ||
+      data.description.toLowerCase().includes(filterValue);
+  }
+
   ngAfterViewInit() {
     this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
