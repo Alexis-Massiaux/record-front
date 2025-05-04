90175cec4bc55a2a2bf3b24827a55e463ccd21fc
Alexis Massiaux <alexis.massiaux@worldline.com>
2025-05-04 18:54:38 +0200

    feat: display date as FR format in datepicker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import {Component} from '@angular/core';
 import {RecordTableComponent} from './record-table/record-table.component';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 
 @Component({
   selector: 'app-root',
   imports:[RecordTableComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'fr-FR' }
+  ],
 })
 export class AppComponent {
   title = 'record-front';
